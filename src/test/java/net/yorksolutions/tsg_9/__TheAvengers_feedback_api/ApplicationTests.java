a91d9382a8c5be14733337f3dd42f62c4a79aac1
Mike Files <m.kefiles@gmail.com>
2025-11-10 12:54:48 -0500

    Spring Project added for API

---
+++ src/test/java/net/yorksolutions/tsg_9/__TheAvengers_feedback_api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.tsg_9.__TheAvengers_feedback_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
