9a70b736c6eefa3913d1d5835ebbda8e0a45dff7
Mike Files <m.kefiles@gmail.com>
2025-11-18 09:00:59 -0500

    Kafka Publisher (with Schema thru Avro) complete

--- src/main/java/net/yorksolutions/tsg/feedbackapi/messaging/KafkaProducerConfig.java
+++ src/main/java/net/yorksolutions/tsg/feedbackapi/messaging/KafkaProducerConfig.java
@@ -1,4 +1,54 @@
 package net.yorksolutions.tsg.feedbackapi.messaging;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
 public class KafkaProducerConfig {
+
+    @Value("${topic.name}")
+    private String topicName;
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.properties.schema.registry.url}")
+    private String schemaRegistryUrl;
+
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(
+                org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapServers
+        );
+        props.put(
+                org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                org.apache.kafka.common.serialization.StringSerializer.class
+        );
+        props.put(
+                org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                io.confluent.kafka.serializers.KafkaAvroSerializer.class
+        );
+        props.put("schema.registry.url", schemaRegistryUrl);
+        props.put("auto.register.schemas", true);
+        return props;
+    }
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
 }
